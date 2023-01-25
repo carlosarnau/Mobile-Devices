846ec8519f5e1ddd4c551cd38596d874f13dbffa
carlosarnau <arnau.carlos01@gmail.com>
2023-01-26 00:08:36 +0100

    Deleting no working thins

--- finalmobiles/lib/screens/SecondScreen.dart
+++ finalmobiles/lib/screens/SecondScreen.dart
@@ -8,29 +8,6 @@ class SecondScreen extends StatelessWidget {
       title: 'Second Screen',
       home: Scaffold(
         appBar: AppBar(title: const Text('Firebase tasks')),
-        body: StreamBuilder(
-          stream: FirebaseFirestore.instance.collection('tasks').snapshots(),
-          builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
-            if (!snapshot.hasData) {
-              return const Center(child: CircularProgressIndicator());
-            }
-            List<DocumentSnapshot> docs = snapshot.data!.docs;
-            return ListView.builder(
-              itemCount: docs.length,
-              itemBuilder: (context, index) {
-                Map<String, dynamic> data =
-                    docs[index].data as Map<String, dynamic>;
-                return ListTile(
-                  leading: Checkbox(
-                    value: data['state'],
-                    onChanged: (bool? value) {},
-                  ),
-                  title: Text(data['name']),
-                );
-              },
-            );
-          },
-        ),
       ),
     );
   }
